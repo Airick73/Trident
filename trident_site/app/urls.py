80e6cd2ce32e526373d4cca4d141a3dd9043f075
Airick73 <72322466+Airick73@users.noreply.github.com>
2022-06-18 22:48:40 -0400

    working on center brand and video loop

---
+++ trident_site/app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app import views
+
+app_name = 'app'
+
+urlpatterns=[
+    # path('carrers/', views.carrers, name = 'carrers'),
+    # path('about_me/', views.about_me, name = 'about_me'),
+]
